a4955f40e22146f642df1a6e0534b986ff42f5fe
Caique Lopes <caiquecriado18@gmail.com>
2024-02-22 08:43:24 -0300

    alteração na logica do login component para navegação das telascom implementação do back-end

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,45 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiServiceLogin } from '../api.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Route } from '@angular/router';
-import { NavigationExtras } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  user = {
-    username: '',
-    password: ''
-  };
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor(private apiService: ApiServiceLogin, private route: Route, private router: ActivatedRoute, private navigate: NavigationExtras) {}
+  constructor(private apiService: ApiServiceLogin, private router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   enviarDadosLogin() {
-    this.apiService.enviarDadosLogin(this.user).subscribe(
-      (response) => {
-        this.route.navigate(['/login'])
-      },
-      
-      (error) => {
-        console.error('Erro ao enviar dados:', error);
-      }
-    );
-  }
-  enviarDadoscadastro() {
-    this.apiService.enviarDadoscadastro(this.user).subscribe(
-      (response) => {
-        console.log('Dados enviados com sucesso:', response);
-        // Lógica adicional após o envio bem-sucedido, se necessário
-      },
-      
-      (error) => {
-        console.error('Erro ao enviar dados:', error);
-      }
-    );
+    if (this.loginForm.valid) {
+      this.apiService.enviarDadosLogin(this.loginForm.value).subscribe(
+        (response) => {
+          this.router.navigate(['/login']);
+        },
+        
+        (error) => {
+          console.error('Erro ao enviar dados:', error);
+        }
+      );
+    } else {
+      console.log('Formulário inválido');
+    }
   }
 
+  enviarDadoscadastro() {
+    if (this.loginForm.valid) {
+      this.apiService.enviarDadoscadastro(this.loginForm.value).subscribe(
+        (response) => {
+          console.log('Dados enviados com sucesso:', response);
+        },
+        
+        (error) => {
+          console.error('Erro ao enviar dados:', error);
+        }
+      );
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
